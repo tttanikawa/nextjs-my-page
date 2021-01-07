2c1ba6fe71f7404993217c791ccf3d2c03f36482
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2021-01-07 23:37:01 +0900

    Update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,39 +3,63 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 type Props = {
-  children?: ReactNode
-  title?: string
+    children?: ReactNode
+    title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+const Layout = ({ children, title = 'ukiyoyo' }: Props) => (
+    <div>
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"></link>
+            <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+        </Head>
+        <header>
+            <nav className="navbar is-spaced" role="navigation" aria-label="main navigation">
+                <div className="container">
+                    {/* <div className="navbar-brand">
+                        <a className="navbar-item">
+                            ukiyoyo
+                        </a>
+                    </div> */}
+
+                    <div className="navbar-menu is-active">
+                        <div className="navbar-end">
+                            <Link href="/">
+                                <a className="navbar-item">
+                                    Home
+                            </a>
+                            </Link>
+
+                            <Link href="/about">
+                                <a className="navbar-item">
+                                    Blog
+                            </a>
+                            </Link>
+
+                            <Link href="/users">
+                                <a className="navbar-item">
+                                    Users List
+                            </a>
+                            </Link>
+
+                            <Link href="/api/users">
+                                <a className="navbar-item">
+                                    api
+                            </a>
+                            </Link>
+                            <a> &nbsp;</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+
+        </header>
+        {children}
+    </div>
 )
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,112 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+    <Layout title="ukiyoyo.dev">
+        <section className="hero is-info is-bold">
+            <div className="hero-body">
+                <div className="container">
+
+                    <h1 className="title is-2">
+                        <strong>ukiyoyo</strong>
+                    </h1>
+                    <h2 className="subtitle">
+                        Software Engineer at DENSO
+                    </h2>
+                </div>
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <div className="subtitle is-3 has-text-weight-bold">
+                    Experience
+                </div>
+                <p>
+                    2018.4 - <br />
+                    <strong>Software Engineer at DENSO Corp.</strong><br />
+                    Development of image recognition algorithms for Advanced Driving Assistant System (ADAS) <br />
+                    <div className="tags">
+                        <span className="tag">C/C++</span>
+                        <span className="tag">Python</span>
+                        <span className="tag">Matlab</span>
+                        <span className="tag">Docker</span>
+                        <span className="tag">Windows</span>
+                    </div>
+
+                    2016.9 - 2017.3<br />
+                    <strong>Part-time engineer at Nagoya University</strong><br />
+                    Development of OCR systems to recognize hand-written characters, which is used in the university<br />
+                    <div className="tags">
+                        <span className="tag">Python</span>
+                        <span className="tag">Linux</span>
+                        <span className="tag">macOS</span>
+                    </div>
+                </p>
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <div className="subtitle is-3 has-text-weight-bold">
+                    Education
+                </div>
+                <p>
+                    2016.4 - 2018.3<br />
+                    <strong>Master of Information Science, Nagoya University</strong><br />
+                    Research of image recognition, especially in multi-object tracking<br />
+                    <div className="tags">
+                        <span className="tag">Python</span>
+                        <span className="tag">Matlab</span>
+                        <span className="tag">Linux</span>
+                        <span className="tag">macOS</span>
+                    </div>
+
+                    2012.4 - 2016.3<br />
+                    <strong>Bachelor of Engineering, Nagoya University</strong><br />
+                    Research of image recognition, especially in object detection and tracking<br />
+                    <div className="tags">
+                        <span className="tag">C/C++</span>
+                        <span className="tag">Linux</span>
+                        <span className="tag">Windows</span>
+                    </div>
+                </p>
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <div className="subtitle is-3 has-text-weight-bold">
+                    Publications
+                </div>
+                <p>
+                    <Link href="https://www.murase.m.is.nagoya-u.ac.jp/~tanikawau/publication.html">
+                        <a>Research papers</a>
+                    </Link>
+                </p>
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <h4 className="subtitle is-3 has-text-weight-bold">
+                    Links
+                </h4>
+                <p>
+                    <Link href="https://twitter.com/_ukiyoyo">
+                        <a>Twitter</a>
+                    </Link>
+                    <br />
+                    <Link href="https://github.com/ukiyoyo">
+                        <a>GitHub</a>
+                    </Link>
+                    <br />
+                    <Link href="https://atcoder.jp/users/ukiyo">
+                        <a>AtCoder</a>
+                    </Link>
+                </p>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default IndexPage
